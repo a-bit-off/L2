00d9b0580335fab4dffa3229444fda7265a8e5af
a-bit-off <abitovtemirkan@gmail.com>
2023-08-18 08:57:16 +0300

    3

--- pattern/cmd/main.go
+++ pattern/cmd/main.go
@@ -1,8 +1,21 @@
 package main
 
-import "pattern"
+import (
+	"fmt"
+	"pattern"
+)
 
 func main() {
+	fmt.Println("Facade:")
 	pattern.RunFacade()
+	fmt.Println()
+
+	fmt.Println("Builder:")
 	pattern.RunBuilder()
+	fmt.Println()
+
+	fmt.Println("Visitor:")
+	pattern.RunVisitor()
+	fmt.Println()
+
 }
