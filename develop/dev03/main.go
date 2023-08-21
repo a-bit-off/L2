43b4d0ab0dab4455838f6ab792e3206e6fec751d
a-bit-off <abitovtemirkan@gmail.com>
2023-08-21 16:53:11 +0300

    03 done

--- develop/dev03/main.go
+++ develop/dev03/main.go
@@ -38,11 +38,18 @@ import (
 // lowercase lc
 // uppercase uc
 
+// Args ...
 type Args struct {
-	lcK      int
-	lcN      bool
-	lcR      bool
-	lcU      bool
+	lcK int
+	lcN bool
+	lcR bool
+	lcU bool
+
+	ucM bool
+	lcB bool
+	lcC bool
+	lcH bool
+
 	filePath string
 }
 
@@ -53,7 +60,7 @@ func main() {
 	}
 	data, err := getData(args.filePath)
 	if err != nil {
-		log.Println(err)
+		log.Fatalln(err)
 	}
 
 	res := mySort(args, data)
@@ -67,15 +74,31 @@ func getArgs() (*Args, error) {
 	n := flag.Bool("n", false, "сортировать по числовому значению")
 	r := flag.Bool("r", false, "сортировать в обратном порядке")
 	u := flag.Bool("u", false, "не выводить повторяющиеся строки")
+
+	M := flag.Bool("M", false, "сортировать по названию месяца")
+	b := flag.Bool("b", false, "игнорировать хвостовые пробелы")
+	c := flag.Bool("c", false, "проверять отсортированы ли данные")
+	h := flag.Bool("h", false, "сортировать по числовому значению с учётом суффиксов")
 	flag.Parse()
 
 	if len(os.Args) == 1 {
-		return nil, fmt.Errorf("Укажите файл!")
+		return nil, fmt.Errorf("укажите файл")
 	}
 	filePath := os.Args[len(os.Args)-1]
-	//filePath := "files/case1_k.txt"
 
-	return &Args{lcK: *k, lcN: *n, lcR: *r, lcU: *u, filePath: filePath}, nil
+	return &Args{
+		lcK: *k,
+		lcN: *n,
+		lcR: *r,
+		lcU: *u,
+
+		ucM: *M,
+		lcB: *b,
+		lcC: *c,
+		lcH: *h,
+
+		filePath: filePath,
+	}, nil
 }
 
 func getData(filePath string) ([]string, error) {
@@ -111,9 +134,29 @@ func mySort(args *Args, data []string) []string {
 	if args.lcU {
 		res = flagLcU(data)
 	}
+
+	if args.ucM {
+		res = flagUcM(data)
+	}
+	if args.lcB {
+		res = flagLcB(data)
+	}
+	if args.lcC {
+		c := flagLcC(data)
+		fmt.Printf("sort: %s: flag -c: %t\n", args.filePath, c)
+		if !c {
+			return nil
+		}
+
+	}
+	if args.lcH {
+		res = flagLcH(data)
+	}
+
 	return res
 }
 
+// main flags
 func flagLcK(data []string, args *Args) []string {
 	position := args.lcK
 	if position <= 0 {
@@ -167,8 +210,86 @@ func flagLcU(data []string) []string {
 		myMap[data[i]] = i
 	}
 
-	for k, _ := range myMap {
+	for k := range myMap {
 		res = append(res, k)
 	}
 	return res
+}
+
+// bonus flags
+func flagUcM(data []string) []string {
+	sort.Slice(data, func(i, j int) bool {
+		return parseMonth(data[i]) < parseMonth(data[j])
+	})
+	return data
+}
+
+func parseMonth(s string) int {
+	var month string
+	fmt.Sscanf(s, "%s", &month)
+
+	switch month {
+	case "Январь":
+		return 1
+	case "Февраль":
+		return 2
+	case "Март":
+		return 3
+	case "Апрель":
+		return 4
+	case "Май":
+		return 5
+	case "Июнь":
+		return 6
+	case "Июль":
+		return 7
+	case "Август":
+		return 8
+	case "Сентябрь":
+		return 9
+	case "Октябрь":
+		return 10
+	case "Ноябрь":
+		return 11
+	case "Декабрь":
+		return 12
+	}
+	return 0
+}
+
+func flagLcB(data []string) []string {
+	sort.Slice(data, func(i, j int) bool {
+		return strings.TrimSpace(data[i]) < strings.TrimSpace(data[j])
+	})
+	return data
+}
+
+func flagLcC(data []string) bool {
+	return sort.SliceIsSorted(data, func(i, j int) bool {
+		return data[i] < data[j]
+	})
+}
+
+func flagLcH(data []string) []string {
+	sort.Slice(data, func(i, j int) bool {
+		return parseSize(data[i]) < parseSize(data[j])
+	})
+	return data
+}
+
+func parseSize(s string) float64 {
+	var size float64
+	var suffix string
+	fmt.Sscanf(s, "%f%s", &size, &suffix)
+	switch suffix {
+	case "K":
+		size *= 1e3
+	case "M":
+		size *= 1e6
+	case "G":
+		size *= 1e9
+	case "T":
+		size *= 1e12
+	}
+	return size
 }
--- develop/dev03/main_test.go
+++ develop/dev03/main_test.go
@@ -1,1 +1,187 @@
 package main
+
+import (
+	"log"
+	"reflect"
+	"testing"
+)
+
+func TestCase1_k_1(t *testing.T) {
+	args := &Args{lcK: 3, filePath: "files/case1_k.txt"}
+	data, err := getData(args.filePath)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	result := mySort(args, data)
+	expected := []string{
+		"2 7",
+		"6 3",
+		"0 9 1",
+		"4 5 2",
+		"8 1 3",
+	}
+
+	if !reflect.DeepEqual(result, expected) {
+		t.Errorf("Expected %v, but got %v", expected, result)
+	}
+}
+
+func TestCase1_k_2(t *testing.T) {
+	args := &Args{lcK: 2, filePath: "files/case1_k.txt"}
+	data, err := getData(args.filePath)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	result := mySort(args, data)
+	expected := []string{
+		"8 1 3",
+		"6 3",
+		"4 5 2",
+		"2 7",
+		"0 9 1",
+	}
+
+	if !reflect.DeepEqual(result, expected) {
+		t.Errorf("Expected %v, but got %v", expected, result)
+	}
+}
+
+func TestCase2_k(t *testing.T) {
+	args := &Args{lcK: 1, filePath: "files/case2_k.txt"}
+	data, err := getData(args.filePath)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	result := mySort(args, data)
+	expected := []string{
+		"012С",
+		"345D",
+		"678A",
+		"902B",
+	}
+
+	if !reflect.DeepEqual(result, expected) {
+		t.Errorf("Expected %v, but got %v", expected, result)
+	}
+}
+
+func TestCase3_n_r_1(t *testing.T) {
+	args := &Args{lcN: true, filePath: "files/case3_n_r.txt"}
+	data, err := getData(args.filePath)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	result := mySort(args, data)
+	expected := []string{
+		"1",
+		"2",
+		"3",
+		"4",
+		"4",
+		"5",
+		"6",
+		"9",
+	}
+
+	if !reflect.DeepEqual(result, expected) {
+		t.Errorf("Expected %v, but got %v", expected, result)
+	}
+}
+
+func TestCase3_n_r_2(t *testing.T) {
+	args := &Args{lcR: true, filePath: "files/case3_n_r.txt"}
+	data, err := getData(args.filePath)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	result := mySort(args, data)
+	expected := []string{
+		"9",
+		"6",
+		"5",
+		"4",
+		"4",
+		"3",
+		"2",
+		"1",
+	}
+
+	if !reflect.DeepEqual(result, expected) {
+		t.Errorf("Expected %v, but got %v", expected, result)
+	}
+}
+
+func TestCase4_u(t *testing.T) {
+	args := &Args{lcU: true, filePath: "files/case4_u.txt"}
+	data, err := getData(args.filePath)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	result := mySort(args, data)
+	expected := []string{
+		"123",
+		"223",
+		"2344",
+	}
+
+	if !reflect.DeepEqual(result, expected) {
+		t.Errorf("Expected %v, but got %v", expected, result)
+	}
+}
+
+func TestCase5_b(t *testing.T) {
+	args := &Args{lcB: true, filePath: "files/case5_b.txt"}
+	data, err := getData(args.filePath)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	result := mySort(args, data)
+	expected := []string{
+		"line 1",
+		" line 2",
+		"  line 3",
+		"line 4",
+	}
+
+	if !reflect.DeepEqual(result, expected) {
+		t.Errorf("Expected %v, but got %v", expected, result)
+	}
+}
+
+func TestCase6_h(t *testing.T) {
+	args := &Args{lcH: true, filePath: "files/case6_h.txt"}
+	data, err := getData(args.filePath)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	result := mySort(args, data)
+	expected := []string{
+		"500B",
+		"1K",
+		"100M",
+		"10G",
+	}
+
+	if !reflect.DeepEqual(result, expected) {
+		t.Errorf("Expected %v, but got %v", expected, result)
+	}
+}
+
+func TestCase7_M(t *testing.T) {
+	args := &Args{ucM: true, filePath: "files/case7_M.txt"}
+	data, err := getData(args.filePath)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	result := mySort(args, data)
+	expected := []string{
+		"Январь",
+		"Март",
+		"Апрель",
+		"Декабрь",
+	}
+
+	if !reflect.DeepEqual(result, expected) {
+		t.Errorf("Expected %v, but got %v", expected, result)
+	}
+}
