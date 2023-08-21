7adacd35c7909ac2db7d51f6bbc77966072b7e32
a-bit-off <abitovtemirkan@gmail.com>
2023-08-21 13:43:15 +0300

    main part done

---
+++ develop/dev03/main.go
@@ -0,0 +1,166 @@
+/*
+=== Утилита sort ===
+
+Отсортировать строки (man sort)
+Основное
+
+Поддержать ключи
+
+- k — указание колонки для сортировки
+- n — сортировать по числовому значению
+- r — сортировать в обратном порядке
+- u — не выводить повторяющиеся строки
+
+Дополнительное
+
+Поддержать ключи
+
+-M — сортировать по названию месяца
+-b — игнорировать хвостовые пробелы
+-c — проверять отсортированы ли данные
+-h — сортировать по числовому значению с учётом суффиксов
+
+Программа должна проходить все тесты. Код должен проходить проверки go vet и golint.
+*/
+
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"sort"
+	"strings"
+)
+
+// lowercase lc
+// uppercase uc
+
+type Args struct {
+	lcK      int
+	lcN      bool
+	lcR      bool
+	lcU      bool
+	filePath string
+}
+
+func main() {
+	args, err := getArgs()
+	if err != nil {
+		log.Fatalln(err)
+	}
+	data, err := getData(args.filePath)
+	if err != nil {
+		log.Println(err)
+	}
+	fmt.Println(args)
+	fmt.Println(data)
+	res := mySort(args, data)
+	for _, r := range res {
+		fmt.Println(r)
+	}
+}
+
+func getArgs() (*Args, error) {
+	k := flag.Int("k", 0, "указание колонки для сортировки")
+	n := flag.Bool("n", false, "сортировать по числовому значению")
+	r := flag.Bool("r", false, "сортировать в обратном порядке")
+	u := flag.Bool("u", false, "не выводить повторяющиеся строки")
+	flag.Parse()
+
+	if len(os.Args) == 1 {
+		return nil, fmt.Errorf("Укажите файл!")
+	}
+	filePath := os.Args[len(os.Args)-1]
+
+	return &Args{lcK: *k, lcN: *n, lcR: *r, lcU: *u, filePath: filePath}, nil
+}
+
+func getData(filePath string) ([]string, error) {
+	f, err := os.Open(filePath)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+
+	scanner := bufio.NewScanner(f)
+	data := make([]string, 0, 10)
+	for scanner.Scan() {
+		data = append(data, scanner.Text())
+	}
+
+	if scanner.Err() != nil {
+		return nil, err
+	}
+	return data, nil
+}
+
+func mySort(args *Args, data []string) []string {
+	res := data
+
+	if args.lcK > 0 {
+		res = flagLcK(res, args.lcK)
+	}
+	if args.lcN {
+		res = flagLcN(res)
+	}
+	if args.lcR {
+		res = flagLcR(res)
+	}
+	if args.lcU {
+		res = flagLcU(data)
+	}
+	return res
+}
+
+func flagLcK(data []string, position int) []string {
+	if position <= 0 {
+		return nil
+	}
+	position--
+	sort.Slice(data, func(i, j int) bool {
+		di := strings.Split(data[i], " ")
+		if len(di) < position {
+			return true
+		}
+
+		dj := strings.Split(data[j], " ")
+		if len(dj) < position {
+			return false
+		}
+
+		return di[position] < dj[position]
+	})
+	return data
+}
+
+func flagLcN(data []string) []string {
+	sort.Slice(data, func(i, j int) bool {
+		return data[i] < data[j]
+	})
+	return data
+}
+
+func flagLcR(data []string) []string {
+	sort.Slice(data, func(i, j int) bool {
+		return data[i] > data[j]
+	})
+	return data
+}
+
+func flagLcU(data []string) []string {
+	size := len(data)
+	res := make([]string, 0, size)
+	myMap := make(map[string]int, size)
+
+	for i := 0; i < size; i++ {
+		myMap[data[i]] = i
+	}
+
+	for k, _ := range myMap {
+		res = append(res, k)
+	}
+	return res
+}
