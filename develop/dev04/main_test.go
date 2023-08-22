398d5fae96541cb44902ba112278cc801a954834
a-bit-off <abitovtemirkan@gmail.com>
2023-08-22 18:06:18 +0300

    test

--- develop/dev04/main_test.go
+++ develop/dev04/main_test.go
@@ -1,1 +1,42 @@
 package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestFindAnagrams(t *testing.T) {
+	words := []string{"cat", "dog", "tac", "god", "act"}
+	expected := map[string][]string{
+		"act": {"act", "cat", "tac"},
+		"dgo": {"dog", "god"},
+	}
+
+	result := findAnagrams(words)
+
+	if !reflect.DeepEqual(result, expected) {
+		t.Errorf("Expected %v, but got %v", expected, result)
+	}
+}
+
+func TestSortString(t *testing.T) {
+	str := "bca"
+	expected := "abc"
+
+	result := sortString(str)
+
+	if result != expected {
+		t.Errorf("Expected %s, but got %s", expected, result)
+	}
+}
+
+func TestGetUnique(t *testing.T) {
+	words := []string{"cat", "cat", "dog", "dog", "dog"}
+	expected := []string{"cat", "dog"}
+
+	getUnique(&words)
+
+	if !reflect.DeepEqual(words, expected) {
+		t.Errorf("Expected %v, but got %v", expected, words)
+	}
+}
